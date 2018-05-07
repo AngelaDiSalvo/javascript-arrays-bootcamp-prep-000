de03aa23e6e2ac75babf4e06508ed49fe142203d
Angela DiSalvo <angela.disalvo@gmail.com>
2018-05-07 22:05:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,6 +17,7 @@ function accessElementInArray(array, element) {
 return array[element]  
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  
+  array.shift()
+  return array
 }
 
