f307692022982b52611302087375445bb9dc216a
Angela DiSalvo <angela.disalvo@gmail.com>
2018-05-07 22:02:50 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(array,element) {
+  return array = [element,...array]
+}
+function destructivelyAddElementToBeginningOfArray (array,element) {
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element) {
+  return array = [...array, element]
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, element) {
+return array[element]  
+}
+
+
